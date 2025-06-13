20a10355a15647ee113c2f704f57ce375e86e900
Adrian Winterstein <adrian@winterstein.biz>
2025-06-13 11:34:52 +0200

    build: script for creating releases
    Added a script for creating releases with the cargo release plugin.

---
+++ create-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")" # Make sure to be in the directory where the script is located
+
+# Ask for the version number of the release
+echo -n "Version: "
+read -r VERSION
+
+# Install the cargo release plugin if it is not yet installed
+if ! cargo --list | grep -q release; then cargo install cargo-release; fi
+
+# Do the release with the "cargo release" command
+cargo release -p tsp-http-client --sign "$@" "$VERSION"
